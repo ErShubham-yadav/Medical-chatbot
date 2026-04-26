bcc59bc61dd517f057964d76386d200021efe6f2
Shubham Yadav <shubhamyadavyv@gmail.com>
2026-04-26 10:14:12 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Shubham Yadav",
+    author_email="shubhamyadavyv@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
